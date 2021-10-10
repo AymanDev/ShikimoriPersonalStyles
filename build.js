7cbb47103db04021364a1d5828111e7f2e5db4c9
AymanDev <aimandeveloper@gmail.com>
2021-10-10 18:17:30 +0300

    fix: build script fix: perfomance measurmenets

--- build.js
+++ build.js
@@ -1,4 +1,3 @@
-"use strict";
 const sass = require("sass");
 const fs = require("fs");
 const path = require("path");
@@ -9,19 +8,28 @@ const packageVersion = process.env.PACKAGE_VERSION;
 const srcFolder = path.join(__dirname, "src");
 const buildFolder = path.join(__dirname, "build");
 
+if (!fs.existsSync(buildFolder)) {
+  fs.mkdirSync(buildFolder);
+  console.log("Build folder created");
+}
+
 try {
+  console.time("Reading SCSS");
   const scssFile = path.join(srcFolder, "index.scss");
   const data = fs.readFileSync(scssFile, "utf8");
   const outputFile = path.join(buildFolder, `index-${version}.css`);
+  console.timeEnd("Reading SCSS");
 
+  console.time("Render SCSS");
   const result = sass.renderSync({
     data: `$VERSION: "${packageVersion}";\n${data}`,
     includePaths: [srcFolder],
   });
+  console.timeEnd("Render SCSS");
 
-  console.time("SASS Compile");
+  console.time("Writing CSS");
   fs.writeFileSync(outputFile, result.css);
-  console.timeEnd("SASS Compile");
+  console.timeEnd("Writing CSS");
 } catch (err) {
   console.error(err);
   process.exit(1);
