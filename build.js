b0eadbf828fe02b4b21e0167ee6dcbea5fb1bfe8
AymanDev <aimandeveloper@gmail.com>
2024-02-09 21:53:39 +0300

    feat: new entries

--- build.js
+++ build.js
@@ -24,6 +24,7 @@ try {
   const result = sass.renderSync({
     data: `$VERSION: "${packageVersion}";\n${data}`,
     includePaths: [srcFolder],
+    charset: false,
   });
   console.timeEnd("Render SCSS");
 
